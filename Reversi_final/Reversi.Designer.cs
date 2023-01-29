633e6eea33fbed1985bbcc3fb3039f43412c82e4
Siebe Tolsma <50704735+siepppie@users.noreply.github.com>
2023-01-29 18:05:30 +0100

    Add files via upload

---
+++ Reversi_final/Reversi.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Reversi_final
+{
+    partial class Reversi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Test = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Test
+            // 
+            this.Test.AutoSize = true;
+            this.Test.Location = new System.Drawing.Point(630, 181);
+            this.Test.Name = "Test";
+            this.Test.Size = new System.Drawing.Size(34, 16);
+            this.Test.TabIndex = 0;
+            this.Test.Text = "Test";
+            // 
+            // Reversi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Test);
+            this.Name = "Reversi";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Test;
+    }
+}
+
---
+++ Reversi_final/Reversi.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Reversi_final
+{
+    public partial class Reversi : Form
+    {
+        static int rows = 6;
+        static int columns = 6;
+
+        int max = rows * columns;
+        int afmetingen = 400 / (Math.Max(columns, rows));
+        int aantal = 4;
+        int kleur = 1;
+        int[,] board = new int[rows, columns];
+
+        Pen pen = new Pen(Color.Black);
+
+        public Reversi()
+        {
+            InitializeComponent();
+            Paint += TekenPanel;
+            MouseClick += ClickPanel;
+            Begin();
+        }
+
+        //Eerste 4 stenen  
+        public void Begin()
+        {
+            int start_x = (rows / 2) - 1;
+            int start_y = (columns / 2) - 1;
+
+            board[start_x, start_y] = 1;
+            board[start_x + 1, start_y] = -1;
+            board[start_x, start_y + 1] = -1;
+            board[start_x + 1, start_y + 1] = 1;
+        }
+
+        //Kliklocatie in array zetten + kleur toevoegen
+        public void ClickPanel(object sender, EventArgs e)
+        {
+            MouseEventArgs mea = (MouseEventArgs)e;
+            Test.Text = "x=" + mea.X.ToString() + " y=" + mea.Y.ToString();
+
+            int x = Convert.ToInt32(Convert.ToDouble((mea.X - 30) / afmetingen));
+            int y = Convert.ToInt32(Convert.ToDouble((mea.Y - 30) / afmetingen));
+
+            if (onBoard(x, y, 0, 0))
+            {
+                if (board[x, y] == 0 && aantal < max)
+                {
+                    board[x, y] = kleur;
+
+                    if (IsValidMove(x, y))
+                    {
+                        Test.Text = "Valid";
+                        drawTiles(x, y);
+                        kleur *= -1;
+                        aantal++;
+                        Invalidate();
+                    }
+                    else
+                    {
+                        Test.Text = "Invalid";
+                    }
+                }
+            }
+        }
+
+        public bool onBoard(int x, int y, int m, int n)
+        {
+            if (x + m >= 0 && x + m <= rows - 1 && y + n >= 0 && y + n <= columns - 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void drawTiles (int x, int y)
+        {
+            for (int m = -1; m <= 1; m++)
+            {
+                for (int n = -1; n <= 1; n++)
+                {
+                    if (!(n == 0 && m == 0) && onBoard(x, y, m, n) && board[x + m, y + n] == kleur * -1)
+                    {
+                        int how_many = 0;
+
+                        while (onBoard(x, y, m, n) && board[x + m, y + n] == kleur * -1)
+                        {
+                            x += m;
+                            y += n;
+
+                            how_many++;
+                        }
+
+                        if (onBoard(x, y, m, n) && board[x + m, y + n] == kleur)
+                        {
+                            for (int p = how_many; p > 0; p--)
+                            {
+                                board[x, y] = kleur;
+                                x -= m;
+                                y -= n;
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+
+        public bool IsValidMove(int x, int y)
+        {
+            for (int m = -1; m <= 1; m++)
+            {
+                for (int n = -1; n <= 1; n++)
+                {
+                    if (!(n == 0 && m == 0) && onBoard(x, y, m, n) && board[x + m, y + n] == kleur * -1)
+                    {
+                        while (onBoard(x, y, m, n) && board[x + m, y + n] == kleur * -1)
+                        {
+                            x += m;
+                            y += n;
+                        }
+
+                        if (onBoard(x, y, m, n) && board[x + m, y + n] == kleur)
+                        {                            
+                            return true;
+                        }
+
+                        return false;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public void TekenPanel(object sender, PaintEventArgs pea)
+        {
+            for (int x = 0; x < rows; x++)
+            {
+                for (int y = 0; y < columns; y++)
+                {
+                    pea.Graphics.DrawRectangle(pen, x * afmetingen + 30, y * afmetingen + 30, afmetingen, afmetingen);
+
+                    if (this.board[x, y] == 1)
+                    {
+                        pea.Graphics.FillEllipse(Brushes.Blue, x * afmetingen + 30, y * afmetingen + 30, afmetingen, afmetingen);
+                    }
+                    else if (this.board[x, y] == -1)
+                    {
+                        pea.Graphics.FillEllipse(Brushes.Red, x * afmetingen + 30, y * afmetingen + 30, afmetingen, afmetingen);
+                    }
+                }
+            }
+        }
+
+    }
+}
